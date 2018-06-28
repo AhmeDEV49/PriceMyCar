5588a5d829fa20be8ddd9bd071aae87c17ad9a92
Rouzem <mathieu.rze@gmail.com>
2018-06-28 11:38:03 +0200

    AnnonceLeboncoin object added

---
+++ app/src/main/java/pricemycar/eni/fr/pricemycar/vehicleRecognition/annonceLeboncoin.java
@@ -0,0 +1,16 @@
+package pricemycar.eni.fr.pricemycar.vehicleRecognition;
+
+public class annonceLeboncoin {
+    String label;
+    String url;
+    String pictureURL;
+    String price;
+
+
+    public void AnnonceLeboncoin (String label, String url, String price)
+    {
+        this.label = label;
+        this.url = url;
+        this.price = price;
+    }
+}
